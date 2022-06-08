86b2e017928b490bfda2f88c0e8f3b4e6f97b12c
YungAddy <gdomino30@gmail.com>
2022-06-08 17:24:34 -0400

    first commit

---
+++ js/main.js
@@ -0,0 +1,5 @@
+console.log(
+ "%cIf you used Developer Tools to find this then follow me on Instagram! @yungaddydev",
+ "font-weight: bold; font-size: 50px;color: #aa81ff  ; text-shadow: 3px 3px 0 #b837ea , 6px 6px 0 #aa11bb  , 9px 9px 0 #90006c  , 12px 12px 0 #56002a  , 15px 15px 0 #25000d  , 18px 18px 0 #47054f  , 21px 21px 0 #15021e "
+);
+// REFERENCE: https://www.telerik.com/blogs/how-to-style-console-log-contents-in-chrome-devtools
