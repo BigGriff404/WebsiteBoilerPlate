a6c60dffcf34cadc9c197c43e622aaf81eb4c556
YungAddy <gdomino30@gmail.com>
2022-06-24 13:31:58 -0400

    Styling Console.log today!

--- js/main.js
+++ js/main.js
@@ -1,5 +1,13 @@
 console.log(
  "%cIf you used Developer Tools to find this, then follow me on Instagram! @yungaddydev",
- "font-weight: bold; font-size: 30px;color: #aa81ff  ; text-shadow: 3px 3px 0 #b837ea , 6px 6px 0 #aa11bb  , 9px 9px 0 #90006c  , 12px 12px 0 #56002a  , 15px 15px 0 #25000d  , 18px 18px 0 #47054f  , 21px 21px 0 #15021e "
+ "display: inline-block; background: linear-gradient(-225deg,#002025,#250600); margin: 1.5em auto; border-radius: 1em; font-weight: bold; padding: 20px; font-size: 30px;color: #aa81ff  ; text-shadow: 3px 3px 0 #b837ea , 6px 6px 0 #aa11bb  , 9px 9px 0 #90006c  , 12px 12px 0 #56002a  , 15px 15px 0 #25000d   "
 );
 // REFERENCE: https://www.telerik.com/blogs/how-to-style-console-log-contents-in-chrome-devtools
+// https://youtu.be/UTFnBsJUN00
+console.group(
+ "%cThings to Remember...",
+ "background: linear-gradient(-225deg,#002025,#250600); color: #ffffff ; font-weight: bold ; padding: 4px ;"
+);
+console.log("Take the Small Victories");
+console.log("God Doesn't Make Mistakes");
+console.groupEnd();
