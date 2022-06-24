1373af63cff464ae2459d5222315a6007c7ee364
YungAddy <gdomino30@gmail.com>
2022-06-24 15:45:52 -0400

    Understanding Console

--- js/main.js
+++ js/main.js
@@ -4,10 +4,43 @@ console.log(
 );
 // REFERENCE: https://www.telerik.com/blogs/how-to-style-console-log-contents-in-chrome-devtools
 // https://youtu.be/UTFnBsJUN00
+
+// const BROTHA = {
+//  name: "Griffin",
+//  age: 26,
+//  ethnicity: "bi-racial",
+//  alsoKnownAs: ["YungAddyDev", "BizquikBandit", "Chickaboom"],
+// };
+
+// console.dir(BROTHA);
+
+const NICKNAMES = [
+ {
+  name: "YungAddyDev",
+  occupation: "Web Developer \uD83D\uDC68\uD83C\uDFFD\u200D\uD83D\uDCBB",
+ },
+ {
+  name: "Chickaboom",
+  occupation:
+   "Boyfriend of 3years \uD83E\uDD1F\uD83C\uDFFD \uD83D\uDC9C \uD83D\uDE18",
+ },
+ { name: "BizquikBandit", occupation: "Underground Rapper \uD83C\uDFA4" },
+];
+
+NICKNAMES.forEach((NICKNAMES) => {
+ console.groupCollapsed(`${NICKNAMES.name}`);
+ console.log(`Griffin is also know as ${NICKNAMES.name}`);
+ console.log(`${NICKNAMES.name} works as a ${NICKNAMES.occupation}`);
+ console.groupEnd();
+});
+
 console.group(
- "%cThings to Remember...",
- "background: linear-gradient(-225deg,#002025,#250600); color: #ffffff ; font-weight: bold ; padding: 4px ;"
+ "%cThings to Remember... \uD83D\uDCAF",
+ "background: linear-gradient(-225deg,#002025,#250600); font-size: 30px; color: #ffffff ; font-weight: bold ; padding: 4px ;"
 );
+
 console.log("Take the Small Victories");
 console.log("God Doesn't Make Mistakes");
 console.groupEnd();
+console.warn("Puttin too much dip on ya chip!")
+console.error("Stop being Lazy & Get to Work!")
