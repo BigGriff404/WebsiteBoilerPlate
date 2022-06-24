f9b981117c709aefa0a0a4742a231365c81abedb
YungAddy <gdomino30@gmail.com>
2022-06-24 13:36:22 -0400

    Adding font-family

--- js/main.js
+++ js/main.js
@@ -1,6 +1,6 @@
 console.log(
  "%cIf you used Developer Tools to find this, then follow me on Instagram! @yungaddydev",
- "display: inline-block; background: linear-gradient(-225deg,#002025,#250600); margin: 1.5em auto; border-radius: 1em; font-weight: bold; padding: 20px; font-size: 30px;color: #aa81ff  ; text-shadow: 3px 3px 0 #b837ea , 6px 6px 0 #aa11bb  , 9px 9px 0 #90006c  , 12px 12px 0 #56002a  , 15px 15px 0 #25000d   "
+ "font-family: Tahoma ; display: inline-block; background: linear-gradient(-225deg,#002025,#250600); margin: 1.5em auto; border-radius: 1em; font-weight: bold; padding: 20px; font-size: 30px;color: #aa81ff  ; text-shadow: 3px 3px 0 #b837ea , 6px 6px 0 #aa11bb  , 9px 9px 0 #90006c  , 12px 12px 0 #56002a  , 15px 15px 0 #25000d   "
 );
 // REFERENCE: https://www.telerik.com/blogs/how-to-style-console-log-contents-in-chrome-devtools
 // https://youtu.be/UTFnBsJUN00
